ed11135e32d7a1b2820f945ab5d95b3de14b6470
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-20 04:54:11 -0500

    Create cleanvals.jl

---
+++ src/type/cleanvals.jl
@@ -0,0 +1,15 @@
+
+function clean(x::DD)
+    if (abs(x.hi) <= epseps1)
+        zero(DD)
+    elseif (abs(x.lo) <= eps(eps(x.hi)))
+        DD(x.hi,0.0)
+    else
+        x
+    end
+end
+
+clean(v::Vector{DD}) = [clean(x) for x in v]
+clean(m::Matrix{DD}) = reshape([clean(x) for x in m],size(m))
+clean(a::Array{DD}) = reshape([clean(x) for x in a],size(a))
+
