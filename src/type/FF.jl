93e865bf390a1228eaeaf93cde1f043e8190f7cb
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-20 03:19:06 -0500

    Update FF.jl

--- src/type/FF.jl
+++ src/type/FF.jl
@@ -1,1 +1,14 @@
 
+#=
+The internal structure is 128 bits given as a magnitude-ordered pair of Float64 values.
+The pair are *presumed* to be in cannonical form, where they do not overlap: hi⊕lo ≡ hi.
+This representation is known as "double-double" (see refs.md)
+=#
+
+#    type FF (FloatFloat)
+
+immutable FF <: Real
+    hi::Float64
+    lo::Float64
+end
+
