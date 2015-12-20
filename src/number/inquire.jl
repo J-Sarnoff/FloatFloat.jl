6105f24bf556acfa14b5bc56bece719f593e65ba
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-20 04:57:05 -0500

    Update inquire.jl

--- src/number/inquire.jl
+++ src/number/inquire.jl
@@ -1,4 +1,10 @@
 
+zero(::Type{DD}) = DD(zero(Float64),zero(Float64))
+zero(a::DD)      = zero(DD)
+one(::Type{DD})  = DD(one(Float64),zero(Float64))
+one(a::DD)       = one(DD) 
+
+
 isnan(a::DD)        = isnan(a.hi)
 isinf(a::DD)        = isinf(a.hi)
 isfinite(a::DD)     = isfinite(a.hi)
