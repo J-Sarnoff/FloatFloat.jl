0ed66e91db7905bbe1a9f92ea0deaa7e0f78284b
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-20 03:31:15 -0500

    Update div.jl

--- src/arith/div.jl
+++ src/arith/div.jl
@@ -1,1 +1,17 @@
 
+# division
+
+function (/){T<:DD}(a::T,b::T)
+  q1 = a.hi / b.hi
+  r  = a - (q1 * b)
+
+  q2 = r.hi / b.hi
+  r = r - (q2 * b)
+
+  q3 = r.hi / b.hi
+
+  q1,q2 = eftSum2inOrder(q1, q2)
+  q1,q2 = eftSum3as2(q1,q2,q3)
+  DD(q1,q2)
+end
+
