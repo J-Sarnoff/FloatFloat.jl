9ff6954a267c52abaedd832ba79b359c9fc1588e
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-20 03:24:18 -0500

    Update inquire.jl

--- src/number/inquire.jl
+++ src/number/inquire.jl
@@ -1,1 +1,10 @@
 
+isnan(a::DD)        = isnan(a.hi)
+isinf(a::DD)        = isinf(a.hi)
+isfinite(a::DD)     = isfinite(a.hi)
+issubnormal(a::DD)  = issubnormal(a.hi)
+
+iszero(a::DD)       = (a.hi == zero(typeof(a.hi)))
+ispos(a::DD)        = (a.hi >= zero(typeof(a.hi)))
+ispoz(a::DD)        = !signbit(a.hi)
+isneg(a::DD)        = signbit(a.hi)
