cad72288fc0ffd4337339e1e0ebab6639ec63883
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-20 03:23:53 -0500

    Update portion.jl

--- src/number/portion.jl
+++ src/number/portion.jl
@@ -1,1 +1,22 @@
 
+sign(a::DD)    = sign(a.hi)
+signbit(a::DD) = signbit(a.hi)
+
+(-)(a::DD)   = DD(-a.hi,-a.lo)
+(abs)(a::DD) = (a.hi >= zero(Float64) ? a : -a)
+
+@inline flipsign(a::DD,b::DD) = DD(flipsign(a.hi,b.hi), flipsign(a.lo,b.hi))
+@inline flipsign(a::DD,b::Float64) = DD(flipsign(a.hi,b), flipsign(a.lo,b))
+@inline flipsign(a::DD,b::Integer) = DD(flipsign(a.hi,b), flipsign(a.lo,b))
+
+@inline function copysign(a::DD,b::Float64)
+    if (b < zero(Float64))
+        a.hi < zero(Float64) ? a : -a
+    else
+        a.hi < zero(Float64) ? -a : a
+    end
+end
+@inline copysign(a::DD,b::Integer) = copysign(a,convert(Float64,b))
+@inline copysign(a::DD,b::DD) = copysign(a,b.hi)
+
+
