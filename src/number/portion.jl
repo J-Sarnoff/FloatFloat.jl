b906bbf08cb231823c9415c4fc116316f536983b
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-20 03:25:39 -0500

    Update portion.jl

--- src/number/portion.jl
+++ src/number/portion.jl
@@ -20,3 +20,14 @@ end
 @inline copysign(a::DD,b::DD) = copysign(a,b.hi)
 
 
+
+function frexp(a::DD)
+    frhi, xphi = frexp(a.hi)
+    frlo, xplo = frexp(a.lo)
+    DD(frhi, ldexp(frlo,xplo-xphi)), xphi
+end
+
+function ldexp(a::DD,xp::Int)
+    DD(ldexp(a.hi,xp),ldexp(a.lo,xp))
+end
+ldexp{I<:Integer}(fx::Tuple{DD,I}) = ldexp(fx...)
