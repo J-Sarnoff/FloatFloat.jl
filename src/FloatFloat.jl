c6934268b720f9cab3846e591ae48ee6287adde9
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-20 04:33:01 -0500

    Update FloatFloat.jl

--- src/FloatFloat.jl
+++ src/FloatFloat.jl
@@ -25,7 +25,7 @@ import Base: convert, promote_rule,             # <--------------
 
 export DD,      # constructor
        fluff,   # softens (zeros low order part if eps(eps(high order part)) >= low order part)
-       nearest2 # construct DD from a BigFloat expression, dd = DD(nearest2(exp(BigFloat(1))))
+       nearest2 # construct DD from a BigFloat expr, dd = DD( nearest2( exp(BigFloat(1)) )... )
 
 
 include("module/ErrorFree.jl/src/ErrorFree.jl")
