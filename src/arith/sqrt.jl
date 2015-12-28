38790480cff2c0302c3a5fe1b03d4f090899dcec
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2015-12-28 07:40:15 -0500

    better sqrt

--- src/arith/sqrt.jl
+++ src/arith/sqrt.jl
@@ -13,21 +13,19 @@ function sqrt(a::FF)
        else
            throw(ArgumentError("sqrt expects a nonnegative base"))
        end
-    elseif (a.hi < 1.0e-15) | (a.hi > 1.0e18)
-        throw(ArgumentError("sqrt arg ($a) outside domain"))
-    end
-
-    if (a.hi < 1.0e-7)  # -log2(1.0e-7) < (1/2) Float64 significand bits
-        return one(FF) / sqrt(one(FF)/a)
     end
 
     # initial approximation to 1/sqrt(a)
-    r = FF(one(Float64)/sqrt(a.hi), zero(Float64))
+    r = FF(eftSqrt(1.0/a.hi)...)
 
-    r = r + divby2( r * (one(FF) - (a*(r*r))) )
-    r = r + divby2( r * (one(FF) - (a*(r*r))) )
-    r = r + divby2( r * (one(FF) - (a*(r*r))) )
+    h = one(FF) - a*(r*r)
+    r = r + divby2(r)*h
 
-    r = a*r
-    divby2(r + a/r)
+    h = one(FF) - a*(r*r)
+    r = r + divby2(r)*h
+
+    h = one(FF) - a*(r*r)
+    r = r + divby2(r)*h
+
+    a*r
 end
